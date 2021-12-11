a5c44fe2bf4e7fc5f744345d645a4512401c456c
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2021-12-11 14:45:48 +0700

    Menambahkan fucntion insert signup ke database

--- controllers/functions.php
+++ controllers/functions.php
@@ -1,11 +1,6 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "psb_online_9");
 
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-
 function query($query)
 {
   global $conn;
@@ -15,4 +10,40 @@ function query($query)
     $rows = $row;
   }
   return  $rows;
+}
+
+function signup($data)
+{
+  global $conn;
+
+  $username = strtolower(stripslashes($data['username']));
+  $email = $data['email'];
+  $password = mysqli_real_escape_string($conn, $data['password']);
+  $confirm_password = mysqli_real_escape_string($conn, $data['confirm_password']);
+
+  $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+
+  if (mysqli_fetch_assoc($result)) {
+    echo "
+    <script>
+      alert('Username Tidak Tersedia');
+    </script>
+    ";
+    return false;
+  }
+
+  if ($password !== $confirm_password) {
+    echo "
+    <script>
+      alert('Confirm Password Tidak Sesuai');
+    </script>
+    ";
+    return false;
+  }
+
+  $password = password_hash($password, PASSWORD_DEFAULT);
+
+  $query = "INSERT INTO users(username,email,password) VALUES ('$username','$email', '$password')";
+  mysqli_query($conn, $query);
+  return mysqli_affected_rows($conn);
 }
--- login/signup.php
+++ login/signup.php
@@ -1,49 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
-    label {
-      display: block;
-    }
-  </style>
-</head>
+if (isset($_POST['submit'])) {
+  if (signup($_POST) > 0) {
+    echo "
+    <script> 
+      alert('Data Berhasil Di Tambahkan');
+      document.location.href = 'index.php';
+    </script>
+    ";
+  } else {
+    echo mysqli_error($conn);
+  }
+}
 
-<body>
-  <h1>Sign UP</h1>
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label for="username">username</label>
-        <input type="text" id="username" name="username">
-      </li>
-      <li>
-        <label for="email">email</label>
-        <input type="text" id="email" name="email">
-      </li>
-      <li>
-        <label for="password">password</label>
-        <input type="text" id="password" name="password">
-      </li>
-      <li>
-        <label for="confirm_password">confirm_password</label>
-        <input type="text" id="confirm_password" name="confirm_password">
-      </li>
-      <li>
-        <p>
-          Have an account?
-          <a href="index.php?page=login">Login</a>
-        </p>
-      </li>
-      <li>
-        <button type="submit" name="submit">Sign Up</button>
-      </li>
-    </ul>
-  </form>
-</body>
+?>
 
-</html>
+<style>
+  label {
+    display: block;
+  }
+</style>
+<h1>Sign UP</h1>
+<form action="" method="POST">
+  <ul>
+    <li>
+      <label for="username">username</label>
+      <input type="text" id="username" name="username">
+    </li>
+    <li>
+      <label for="email">email</label>
+      <input type="text" id="email" name="email">
+    </li>
+    <li>
+      <label for="password">password</label>
+      <input type="text" id="password" name="password">
+    </li>
+    <li>
+      <label for="confirm_password">confirm_password</label>
+      <input type="text" id="confirm_password" name="confirm_password">
+    </li>
+    <li>
+      <p>
+        Have an account?
+        <a href="index.php?page=login">Login</a>
+      </p>
+    </li>
+    <li>
+      <button type="submit" name="submit">Sign Up</button>
+    </li>
+  </ul>
+</form>
