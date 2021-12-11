ba4879224a9a39a8148c1a8705b8975d95e01334
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2021-12-12 01:19:35 +0700

    Menambahkan code untuk login(login.php)

--- login/login.php
+++ login/login.php
@@ -1,15 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+if (isset($_POST['login'])) {
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+
+  $x = mysqli_num_rows($result);
+
+  if ($x > 0) {
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row['password'])) {
+      if ($row['level_id'] == 1) {
+        header("location: admin/index.php");
+      } else if ($row['level_id'] == 2) {
+        header("location: user/index.php");
+      }
+    }
+  }
+  $error = true;
+}
+
+?>
+
 
 <body>
   <h1>Login</h1>
+  <?php if (isset($error)) : ?>
+    <h3 style="color: red;"></h3>
+  <?php endif; ?>
   <form action="" method="POST">
     <ul>
       <li>
@@ -26,10 +46,7 @@
         </p>
       </li>
       <li>
-        <button type="submit" name="submit">Login</button>
+        <button type="submit" name="login">Login</button>
       </li>
     </ul>
   </form>
-</body>
-
-</html>
