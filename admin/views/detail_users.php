05eb7abd026f509b44f2eb72f390fad3b22ddddf
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2021-12-14 01:40:47 +0700

    Menambahkan Fitur untuk admin melihat detail user

--- admin/views/detail_users.php
+++ admin/views/detail_users.php
@@ -1,1 +1,30 @@
+<?php
+
+$users = query("SELECT u.id_user, u.username, u.email, u.password, u.image, l.nama_level
+                FROM users as u
+                INNER JOIN level as l ON u.level_id = l.id_level;");
+
+?>
+
 <h1>DETAIL USERS</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <td>ID User</td>
+    <td>Username</td>
+    <td>Email</td>
+    <td>Password</td>
+    <td>Image</td>
+    <td>Level</td>
+  </tr>
+  <?php foreach ($users as $row) : ?>
+    <tr>
+      <td><?= $row['id_user'] ?></td>
+      <td><?= $row['username'] ?></td>
+      <td><?= $row['email'] ?></td>
+      <td><?= $row['password'] ?></td>
+      <td> <img src="../file/img/<?= $row['image'] ?>" width="50px"></td>
+      <td><?= $row['nama_level'] ?></td>
+    </tr>
+  <?php endforeach ?>
+</table>
--- admin/views/pendaftaran.php
+++ admin/views/pendaftaran.php
@@ -1,6 +1,11 @@
 <?php
 
-$pendaftaran = query("SELECT * FROM pendaftaran");
+$pendaftaran = query("SELECT p.id_pendaftaran, j.nama_jurusan, p.nilai_mtk, p.nilai_ipa, p.nilai_ing, p.nilai_ind, 
+                      p.ijazah, s.nama_status, u.username
+                      FROM pendaftaran as p
+                      INNER JOIN jurusan as j ON p.jurusan_id = j.id_jurusan
+                      INNER JOIN status as s ON p.status_id = s.id_status
+                      INNER JOIN users as u ON p.user_id = u.id_user");
 
 ?>
 
@@ -9,27 +14,27 @@ $pendaftaran = query("SELECT * FROM pendaftaran");
 <table border="1" cellpadding="10" cellspacing="0">
   <tr>
     <td>id_pendaftaran</td>
-    <td>jurusan_id</td>
+    <td>jurusan</td>
     <td>nilai_mtk</td>
     <td>nilai_ipa</td>
     <td>nilai_ing</td>
     <td>nilai_ind</td>
     <td>ijazah</td>
-    <td>status_id</td>
-    <td>user_id</td>
+    <td>status</td>
+    <td>user</td>
     <td>Action</td>
   </tr>
   <?php foreach ($pendaftaran as $row) : ?>
     <tr>
       <td><?= $row['id_pendaftaran'] ?></td>
-      <td><?= $row['jurusan_id'] ?></td>
+      <td><?= $row['nama_jurusan'] ?></td>
       <td><?= $row['nilai_mtk'] ?></td>
       <td><?= $row['nilai_ipa'] ?></td>
       <td><?= $row['nilai_ing'] ?></td>
       <td><?= $row['nilai_ind'] ?></td>
       <td><?= $row['ijazah'] ?></td>
-      <td><?= $row['status_id'] ?></td>
-      <td><?= $row['user_id'] ?></td>
+      <td><?= $row['nama_status'] ?></td>
+      <td><?= $row['username'] ?></td>
       <td><a href="index.php?page=update&id=<?= $row['id_pendaftaran'] ?>">Ubah Status</a></td>
     </tr>
   <?php endforeach ?>
